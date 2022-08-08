918a7418d2cf46d3db8e04e347654899a78d103f
oreffice624 <108616070+oreffice624@users.noreply.github.com>
2022-08-08 11:50:07 +0000

    Rails6-bookers2-debug-main4

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    @book_comment = BookComment.new
+
   end
-  
+
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+  @book = Book.find(params[:book_id])
+  @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
+  @book_comment.destroy
+  @book_comment = BookComment.new
   end
 
   private
@@ -19,5 +22,5 @@ class BookCommentsController < ApplicationController
   end
 
 end
-  
+
 
